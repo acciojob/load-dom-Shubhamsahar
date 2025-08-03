dbd1abd3dd3bf4e166a930c7aa8cdd4d1f179555
Shubhamsahar <kumarshubham765484@gmail.com>
2025-08-03 14:42:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,2 @@
 //your JS code here. If required.
- document.addEventListener('DOMContentLoaded', () => {
-            const textNode = document.createTextNode('DOM load success');
-            document.body.appendChild(textNode);
+ 
